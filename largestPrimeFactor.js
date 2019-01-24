6a0d1264017fae459b050b1858a475cd0f41ec97
WendyRdz <46804367+WendyRdz@users.noreply.github.com>
2019-01-23 18:15:41 -0600

    Update largestPrimeFactor.js

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,30 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+var num = 600851475143,
+    div = 2;
 
-  return primeNumber;
-};
+function LargestPrimeFactor() {
+    var can_divide = false;
+    var n = 2;
+    if (div === 2) {
+        div++;
+    } else {
+        div += 2;
+    }
+    while (n < div) {
+        if (div % n === 0) {
+            can_divide = true;
+        }
+        n++;
+    }
+    if (can_divide) {
+        LargestPrimeFactor();
+    }
+}
+
+while (num > 1) {
+    if (num % div === 0) {
+        num /= div;
+    } else {
+        LargestPrimeFactor();
+    }
+}
+console.log(div);
